455a340084701fad533219ad3c894062eeedceb0
Pranjal-Ux <pranjal2shukla@gmail.com>
2025-06-07 23:29:49 +0530

    Encaptulation-01

---
+++ Encap.java
@@ -0,0 +1,7 @@
+public class Encap {
+    public static void main(String[] args) {
+        proton obj = new proton();
+        obj.setAge(17);
+        System.out.println(obj.getAge());
+    }
+}
---
+++ proton.java
@@ -0,0 +1,13 @@
+
+public class proton {
+     private int age;
+     private String name;
+
+     public void setAge(int x) {
+          age = x;
+     }
+
+     public int getAge() {
+          return age;
+     }
+}
